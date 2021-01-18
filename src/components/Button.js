951a9fba883233557d4d62d1b9e0bad1e902d827
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-01-18 18:49:20 -0500

    Button react component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,19 @@
 import React from "react";
-
 import "components/Button.scss";
 
+
+
+const classnames = require('classnames')
+
 export default function Button(props) {
-   return <></>;
+   let buttonClass = classnames("button", `button--${props.confirm?"confirm":"danger"}`);
+   return <button 
+            className={buttonClass} 
+            disabled = {props.disabled? true: false} 
+            onClick={props.onClick}
+         >
+            {props.children} 
+         
+         </button>;
+   
 }
