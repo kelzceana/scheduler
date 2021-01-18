960d01b928c584e0c7d5e687ac024e63ee57f4f0
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-01-18 18:51:15 -0500

    DayListItem componet

---
+++ src/components/DayListItem.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "components/DayListItem.scss";
+const classnames = require('classnames')
+
+
+export default function DayListItem(props) {
+  //function to format the number of spots left}
+  const formatSpots = function(spotsLeft) {
+    if (spotsLeft === 0) {
+      return "no spots remaining"
+    }
+    if (spotsLeft  < 5) {
+      return `${spotsLeft} spot${spotsLeft>1? "s":""} remaining`;
+    }
+
+    return spotsLeft;
+  }
+
+  // conditional rendering of class
+  
+  let dayListClass;
+  if(props.selected) dayListClass = classnames(`day-list__item--selected`)
+  else dayListClass = classnames(`day-list__item`)
+  if(props.spots === 0) dayListClass = classnames(`day-list__item--full`)
+  
+
+
+  return (
+    <li className={dayListClass} onClick={()=>{props.setDay(props.name)}}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
