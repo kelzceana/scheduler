e714beb5d3082d22030f1977ea3dec34dc46e777
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-01-22 10:27:59 -0500

    added the getInterviewForDays function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,16 +1,13 @@
+
 export function getAppointmentsForDay(state, day) {
   const filteredAppointments = state.days.filter(d => d.name === day);
-
   let appointments = [];
-
   if (filteredAppointments.length){
-    appointments = filteredAppointments[0].appointments.map(x => state.appointment[x]);
+    appointments = filteredAppointments[0].appointments.map(x => state.appointments[x]);
   }
 
   return appointments;
 }
-
-
 
 
 export function getInterview(state, interview) {
@@ -21,9 +18,19 @@ export function getInterview(state, interview) {
     student: interview.student,
     interviewer: state.interviewers[interview.interviewer]
   }
-  
   return interviewObj;
-  
-  
+}
+export function getInterviewersForDay(state, day) {
+  //find day first
+
+ const filteredAppointments = state.days.find(d => d.name === day);
+
+ // access to interviers for that day
+ let interviewers = [];
+ if (!filteredAppointments){
+ return interviewers 
+ }
+ interviewers = filteredAppointments.interviewers.map(x => state.interviewers[x]);
+ return interviewers;
 }
 
