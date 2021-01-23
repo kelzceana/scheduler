1c975bfe46d614a40a93ddd7726e76e3ee35b005
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-01-22 20:05:17 -0500

    modified the state with default or null values

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,7 +7,7 @@ import InterviewerList from "components/InterviewerList";
 
 export default function Form(props) {
    const [name, setName] = useState(props.name || "");
-   const [interviewer, setInterviewer] = useState(props.value || null)
+   const [interviewer, setInterviewer] = useState(props.interviewer || null)
 
    function reset() {
      setName('');
